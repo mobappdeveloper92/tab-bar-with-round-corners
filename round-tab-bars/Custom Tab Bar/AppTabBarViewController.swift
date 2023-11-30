2c7b8583745ad3bbd3c27ec299687ecbe4bfe08a
Faizan Mahmood <f.mahmood@tawkeel.com>
2023-11-30 15:11:27 +0500

    tab bar with side curve and middle button completed
    1. configurable side arcs and middle button
    2.

---
+++ round-tab-bars/Custom Tab Bar/AppTabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AppTabBarViewController.swift
+//  round-tab-bars
+//
+//  Created by Faizan Mahmood on 25/07/2023.
+//
+
+import UIKit
+
+class AppTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ round-tab-bars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  round-tab-bars
+//
+//  Created by Faizan Mahmood on 25/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
